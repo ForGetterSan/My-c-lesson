2e3ebee5288b3338d8656146f3ef7d3cfcdffa4a
Mehmet Sait Bingöl <sait72bingol@gmail.com>
2025-07-28 12:17:00 +0300

    Create subtraction_three_equal_numbers.c
    This C program assigns the same integer value (25) to three variables and performs a subtraction operation in the form mynumber1 - mynumber2 - mynumber3. It then prints each value and the result of the subtraction. The code demonstrates chained assignment, basic arithmetic operations, and formatted output with printf.
    
    It’s useful for understanding how left-to-right evaluation works in subtraction expressions.

---
+++ Veriable/subtraction_three_equal_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int mynumber1, mynumber2, mynumber3,division;
+
+    mynumber1 = mynumber2 = mynumber3 = 25;
+
+    division= mynumber1 - mynumber2 - mynumber3;
+
+    printf("Your number one is: %d\n", mynumber1);
+
+    printf("Your number two is: %d\n", mynumber2);
+
+    printf("Your number three is: %d\n", mynumber3);
+
+    printf("The subtraction of all numbers is: %d\n", division);
+
+
+    return 0;
+}
