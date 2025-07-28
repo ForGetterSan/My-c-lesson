449fde17f1ca1c8a4cdf1e1a44e119939603f4c5
Mehmet Sait Bingöl <sait72bingol@gmail.com>
2025-07-28 12:54:17 +0300

    division_two_user_inputs.c
    This C program prompts the user to input two integers, divides the first number by the second, and prints the result. It demonstrates basic input handling, division operation, and output formatting with printf. This example helps beginners understand integer division and user interaction in C.

---
+++ C-input-examples/division_two_user_inputs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int main () {
+int mynumber1,mynumber2,Division;
+printf("enter a first number:");
+scanf("%d",&mynumber1);
+
+printf("enter a second number:");
+scanf("%d",&mynumber2);
+
+Division=mynumber1 / mynumber2;
+
+printf("your sum is:%d",Division);
+return 0;
+}
