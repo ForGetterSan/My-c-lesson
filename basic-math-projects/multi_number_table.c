ae68dcca238d9523366985d2b63161e861980d32
Mehmet Sait Bingöl <sait72bingol@gmail.com>
2025-07-28 14:52:15 +0300

    Create multi_number_table.c
    This C program asks the user to enter five integers. For each number, it calculates and displays its square and cube in a well-formatted table. It's a great beginner-level exercise to practice loops (can be extended), repeated operations, and aligned output formatting using printf.

---
+++ basic-math-projects/multi_number_table.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int mynumber1,mynumber2,mynumber3,mynumber4,mynumber5;
+
+    printf("Enter a first number:");
+    scanf("%d",&mynumber1);
+
+    printf("Enter a second number:");
+     scanf("%d", &mynumber2);
+
+     printf("Enter a third number:");
+      scanf("%d", &mynumber3);
+
+      printf("Enter a fourth number:");
+         scanf("%d",&mynumber4);
+
+        printf("Enter a fifth number:");
+
+                  scanf("%d",&mynumber5);
+
+    printf("the number is:\t\t");
+    printf("square of number:\t\t");
+    printf("cube of number:\n");
+
+    printf("--------------\t\t");
+    printf("------------------\t\t");
+    printf("---------------\n");
+
+    printf("%d\t\t\t", mynumber1);
+    printf("%d\t\t\t", mynumber1 * mynumber1);
+    printf("%d\n", mynumber1 * mynumber1 * mynumber1);
+
+    printf("%d\t\t\t", mynumber2);
+    printf("%d\t\t\t", mynumber2* mynumber2);
+        printf("%d\n", mynumber2 * mynumber2 * mynumber2);
+
+
+        printf("%d\t\t\t", mynumber3);
+            printf("%d\t\t\t", mynumber3 * mynumber3);
+            printf("%d\n", mynumber3 * mynumber3 * mynumber3);
+
+
+            printf("%d\t\t\t", mynumber4);
+                printf("%d\t\t\t", mynumber4 * mynumber4);
+                printf("%d\n", mynumber4 * mynumber4 * mynumber4);
+
+
+
+
+                printf("%d\t\t\t", mynumber5);
+                 printf("%d\t\t\t", mynumber5 * mynumber5);
+                 printf("%d\n", mynumber5 * mynumber5 * mynumber5);
+
+    return 0;
+}
