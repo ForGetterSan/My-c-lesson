3d7742c5d1ccb5d9dbc972206cff144c12cc2a3d
Mehmet Sait Bingöl <sait72bingol@gmail.com>
2025-07-28 12:30:56 +0300

    sum_two_user_inputs.c
    This simple C program prompts the user to enter two integers, adds them, and displays the result. It demonstrates how to read user input using scanf, perform basic addition, and display the output with printf. This is a classic beginner-level example for understanding input/output and arithmetic in C.

---
+++ C-input-examples/sum_two_user_inputs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int main () {
+  
+int mynumber1,mynumber2,sum;
+  
+printf("enter a first number:");
+  
+scanf("%d",&mynumber1);
+
+printf("enter a second number:");
+  
+scanf("%d",&mynumber2);
+
+sum=mynumber1 + mynumber2;
+
+printf("your sum is:%d",sum);
+return 0;
+}
