5a36e08aadb10629b4e93aa5c4042360d5cf9351
Mehmet Sait Bingöl <sait72bingol@gmail.com>
2025-07-28 12:26:53 +0300

    multiplication_three_equal_numbers.c
    This C program assigns the same integer value (5) to three variables and calculates the product of those numbers using multiplication. It prints each value and the final result. This example demonstrates chained assignment and basic multiplication operations using integers in C, along with formatted output using printf.

---
+++ Veriable/multiplication_three_equal_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int mynumber1, mynumber2, mynumber3,multiplication;
+
+    mynumber1 = mynumber2 = mynumber3 = 5;
+
+    multiplication= mynumber1 * mynumber2 * mynumber3;
+
+    printf("Your number one is: %d\n",mynumber1);
+
+    printf("Your number two is: %d\n",mynumber2);
+
+    printf("Your number three is: %d\n",mynumber3);
+
+    printf("The multiplication of all numbers is: %d\n",multiplication);
+
+
+    return 0;
+}
