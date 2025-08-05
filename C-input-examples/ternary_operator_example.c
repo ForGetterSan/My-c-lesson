7a27e9b0425efe456846d663955dabcd83017261
Mehmet Sait Bingöl <sait72bingol@gmail.com>
2025-08-05 13:49:40 +0300

    Create ternary_operator_example.c
    This C program demonstrates the use of the ternary operator to check if a number is greater than 60. It prints "Good work" if true, otherwise it prints "Failed". The program showcases a concise way of implementing conditional statements in C.

---
+++ C-input-examples/ternary_operator_example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    int mynumber;
+    mynumber = 50;
+
+    (mynumber > 60) ? printf("Good work\n") : printf("Failed\n");
+
+    return 0;
+}
