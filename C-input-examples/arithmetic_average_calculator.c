0ad6f6929dd9da852346ee878565919a6e0f28e1
Mehmet Sait Bingöl <sait72bingol@gmail.com>
2025-08-05 12:19:27 +0300

    Create arithmetic_average_calculator.c
    This C program calculates the arithmetic average of four numbers entered by the user. It reads the values using a single scanf call, computes the average, and displays the result. A simple example of user input, arithmetic operations, and formatted output in C.

---
+++ C-input-examples/arithmetic_average_calculator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    float a, b, c, d, average;
+
+    printf("Enter 4 numbers: ");
+    scanf("%f%f%f%f", &a, &b, &c, &d);
+
+    average = (a + b + c + d) / 4;
+
+    printf("The arithmetic average of the numbers you entered is: %f\n", average);
+
+    return 0;
+}
