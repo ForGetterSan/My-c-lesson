c845f450a2221e8fc706063872c0083e79ccdcc1
Mehmet Sait Bingöl <sait72bingol@gmail.com>
2025-08-11 15:48:16 +0300

    Create prime_numbers_up_to_n.c
    A C program that prints all prime numbers up to a user-specified number using nested for loops. 
    It checks each number from 2 up to the input number and prints it if it is prime.

---
+++ my_evolution.c/prime_numbers_up_to_n.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+int mynumber,i,j,control;
+printf("enter a number:");
+scanf("%d",&mynumber);
+
+for(i=2;i<=mynumber;i++){
+	control=1;
+	for(j=2;j<i/2;j++){
+		if(i % j ==0){
+		control=0;
+		break;
+
+	}
+}
+if(control !=0){
+printf("%d\n",i);
+}
+}
+return 0;
+}
