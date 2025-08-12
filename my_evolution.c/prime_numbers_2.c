edf7b6fcd4e9350058f50aa62cd63ee79f252143
Mehmet Sait Bingöl <sait72bingol@gmail.com>
2025-08-12 09:43:43 +0300

    Update prime_numbers_2.c

--- my_evolution.c/prime_numbers_2.c
+++ my_evolution.c/prime_numbers_2.c
@@ -1,39 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main() {
-    int i;
-    float sumgrade, average, examgrade;
+int main () {
+    int mynumber,i,j,control;
+    printf("Bir sayı giriniz: ");
+    scanf("%d",&mynumber);
 
-    sumgrade = 0.0;
-    examgrade = 0.0;
-    i = 1; // Başlangıç değeri
-
-    do {
-        printf("%dth exam grade: ", i);
-        scanf("%f", &examgrade);
-
-        if (examgrade == 0) {
-            break; // 0 girilirse çık
+    for(i=2;i<=mynumber;i++){
+        control=1;
+        for(j=2;j<=sqrt(i);j++){
+            if(i % j == 0){
+                control=0;
+                break;
+            }
         }
-
-        if (examgrade < 0) {
-            printf("Please enter a positive number.\n");
-            continue; // Negatifse başa dön
-        } else {
-            sumgrade += examgrade; // Toplama ekle
+        if(control != 0){
+            printf("%d\n",i);
         }
-
-        i++;
-    } while (examgrade != 0);
-
-    if (i > 1) { // En az bir not girilmişse
-        average = sumgrade / (i - 1);
-        printf("\nYou entered %d exam grades\n", i - 1);
-        printf("Average: %.2f\n", average);
-    } else {
-        printf("No grades entered.\n");
     }
-
     return 0;
 }
