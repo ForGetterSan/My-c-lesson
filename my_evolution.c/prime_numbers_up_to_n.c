54dfa6af078a57ca6f6eb5d5e644fdffc715ed97
Mehmet Sait Bingöl <sait72bingol@gmail.com>
2025-08-12 09:53:42 +0300

    Update prime_numbers_up_to_n.c
    "This code in the file really challenged me a lot. I was just starting to learn while loops, and this code felt like an evolution for me. Just like Ülker takes a leap forward, this code was a major breakthrough for me in programming. It was truly tough, thank you. This code will hold a special place for me, so I’m setting it apart from my other codes."

--- my_evolution.c/prime_numbers_up_to_n.c
+++ my_evolution.c/prime_numbers_up_to_n.c
@@ -2,37 +2,24 @@
 #include <stdlib.h>
 
 int main() {
-    int i;
-    float sumgrade, average, examgrade; 
+    int i, j, control, mynumber;
 
-    sumgrade = 0.0;
-    examgrade = 0.0;
-    i = 1;
+    printf("enter a number: ");
+    scanf("%d", &mynumber);
 
-    do {
-        printf("%dth exam grade: ", i);
-        scanf("%f", &examgrade);
+    for (i = 2; i <= mynumber; i++) {
+        control = 1;
 
-        if (examgrade == 0) {
-            break; 
+        for (j = 2; j <= i / 2; j++) { 
+            if (i % j == 0) {
+                control = 0;
+                break;
+            }
         }
 
-        if (examgrade < 0) {
-            printf("Please enter a positive number.\n");
-            continue; 
-        } else {
-            sumgrade += examgrade; 
+        if (control != 0) {
+            printf("%d\n", i);
         }
-
-        i++;
-    } while (examgrade != 0);
-
-    if (i > 1) { 
-        average = sumgrade / (i - 1);
-        printf("\nYou entered %d exam grades\n", i - 1);
-        printf("Average: %.2f\n", average);
-    } else {
-        printf("No grades entered.\n");
     }
 
     return 0;
