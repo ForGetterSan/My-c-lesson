8e504a14a692a49d982f6b316d386638526da04e
Mehmet Sait Bingöl <sait72bingol@gmail.com>
2025-08-14 10:44:30 +0300

    Create inverted_number_triangle.c
    A C program that prints an inverted number triangle using nested for loops. 
    The first row contains the number 1 repeated, and each subsequent row increases the number while decreasing the count.

---
+++ C-input-examples/inverted_number_triangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i, j, number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    for (i = 0; i <= number; i++) {
+        for (j = 1; j <= number - i; j++) {
+            printf("%d", i + 1);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
