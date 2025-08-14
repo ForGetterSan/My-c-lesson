244e5642657f57c2f434e93c689286b3d66cb997
Mehmet Sait Bingöl <sait72bingol@gmail.com>
2025-08-14 12:03:28 +0300

    Update hollow_square_pattern.c
    A C program that prints a hollow square pattern using nested for loops. 
    Asterisks are printed along the borders, while the inside of the square is filled with spaces.

--- C-input-examples/hollow_square_pattern.c
+++ C-input-examples/hollow_square_pattern.c
@@ -9,3 +9,14 @@ int main() {
 
     for (i = 1; i <= number; i++) {
         for (j = 1; j <= number; j++) {
+            if (i == 1 || i == number || j == 1 || j == number) {
+                printf("* ");
+            } else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
